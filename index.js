2ba31ec8dbca30d8bea705b749682e9e8cb40f56
Akshay P <notakshayp@gmail.com>
2022-08-17 14:52:58 +0530

    dark mode

--- index.js
+++ index.js
@@ -44,9 +44,66 @@
 
 
 
+const toggleTheme=()=>{
+  let icon_nav_bar="";
+  let style_text="";
+
+  if(theme==="light"){
+    theme="dark";
+    icon_nav_bar="fa-sun-o";
+    
+    document.getElementById("theme-icon").classList.remove('fa-moon-o');
+    document.getElementById("theme-icon").classList.add('fa-sun-o');
+
+    style_text="scroll-behavior: smooth;    -webkit-filter: invert(100%);    -moz-filter: invert(100%);    -o-filter: invert(100%);    -ms-filter: invert(100%);";
+    
+
+  }
+  else{
+    theme="light";
+    icon_nav_bar="fa-moon-o";
+    
+    document.getElementById("theme-icon").classList.remove('fa-sun-o');
+    document.getElementById("theme-icon").classList.add('fa-moon-o');
+
+    style_text="scroll-behavior: smooth;";
+
+  }
+
+  var myDiv = document.getElementsByTagName("html")[0];
+  myDiv.setAttribute("style", style_text);
+  document.cookie = `theme=${theme};`;
+}
+
+function getCookie (name) {
+	let value = `; ${document.cookie}`;
+	let parts = value.split(`; ${name}=`);
+	if (parts.length === 2) return parts.pop().split(';').shift();
+}
+
+
+
+
+
+
+  let theme=getCookie('theme');
+  if(theme===undefined){
+    theme="light";
+  }
+
+  if(theme=="dark"){
+    //setting theme on load
+    theme="light";
+    toggleTheme()
+  }
+
+
+
   if(document.getElementById('projects-list-div')){
     renderProjects(false);
   }
   else if(document.getElementById('projects-featured-list-div')){
     renderProjects(true)
   }
+
+document.getElementById('theme').addEventListener("click",toggleTheme)

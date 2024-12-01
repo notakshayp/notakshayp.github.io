1a1373cc8f064b2640ea91fe40907c33815139a9
Akshay <notakshayp@gmail.com>
2024-12-02 00:47:01 +0530

    updated v2

--- portfolio/src/app/app.routes.ts
+++ portfolio/src/app/app.routes.ts
@@ -1,7 +1,3 @@
 import { Routes } from '@angular/router';
-import { WorkInProgressComponent } from './work-in-progress/work-in-progress.component';
 
-export const routes: Routes = [
-    { path: 'WIP', component: WorkInProgressComponent },
-    { path: '**', redirectTo: 'WIP' }
-];
+export const routes: Routes = [];
--- portfolio/tailwind.config.js
+++ portfolio/tailwind.config.js
@@ -1,10 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{html,ts}"
-  ],
+  content: ["./src/**/*.{html,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        text:"#00190C",
+        background:"#F0FEF6",
+        primary:"#21F35E",
+        secondary:"#DC81F1",
+        accent:"#9D4CFD"
+      }
+    },
   },
   plugins: [],
 }

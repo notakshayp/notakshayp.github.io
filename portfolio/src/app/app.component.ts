ee3ada255b5d52c5371525b08a616bbb7dba392a
Akshay <notakshayp@gmail.com>
2024-12-02 18:38:21 +0530

    added nav bar

--- portfolio/src/app/app.component.ts
+++ portfolio/src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { WipComponent } from './components/wip/wip.component';
+import { NavComponent } from './components/nav/nav.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,WipComponent,NavComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ portfolio/src/app/components/wip/wip.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-wip',
+  standalone: true,
+  imports: [],
+  templateUrl: './wip.component.html',
+  styleUrl: './wip.component.css'
+})
+export class WipComponent {
+
+}

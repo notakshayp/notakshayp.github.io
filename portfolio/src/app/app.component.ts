d26d6423a54cf41466430454b20cf26b7e1ffffc
Akshay <notakshayp@gmail.com>
2024-12-08 18:25:49 +0530

    minro changes for load time optimisation

--- portfolio/src/app/app.component.ts
+++ portfolio/src/app/app.component.ts
@@ -4,17 +4,19 @@ import { WipComponent } from './components/wip/wip.component';
 import { NavComponent } from './components/nav/nav.component';
 import { LandingComponent } from './components/landing/landing.component';
 import { AboutmeComponent } from './components/aboutme/aboutme.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faCircleHalfStroke } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,WipComponent,NavComponent, LandingComponent,AboutmeComponent],
+  imports: [RouterOutlet,WipComponent,NavComponent, LandingComponent,AboutmeComponent,FontAwesomeModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'nap-portfolio';
-
+  faCircleHalfStroke=faCircleHalfStroke;
   toggleDarkMode = ():void => {
     document.documentElement.classList.toggle('dark');
   };
